32feab53c155a1f289022bd94c8795323d24dc50
ambitiousTortoise442 <114966048+ambitiousTortoise442@users.noreply.github.com>
2022-11-14 18:59:54 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useState } from "react";
+import BakeryItem from "./components/BakeryItem";
 import bakeryData from "./assets/bakery-data.json";
-
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
@@ -11,18 +11,53 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, updateCart] = useState([]);
+  const [total, updateTotal] = useState(0);
+  const cartDict = new Map();
+  function addTotal(item) {
+    updateTotal(total + item.price)
+  }
 
+  function addCart(item) {
+    updateCart([...cart, item]);
+
+    if (cartDict.has(item)) {
+      let value = cartDict.get(item);
+      value += 1;
+      cartDict.set(item, value)
+    } else {
+      cartDict.put(item, 1);
+    }
+  }
+
+  const handleClick = (item) => {
+    addTotal(item)
+    addCart(item)
+  }
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <div className="Bakery">
+        <div className="BakeryItem">
+          <h1>Jason's Bakery</h1>
+          {bakeryData.map((item, index) => (
+            <BakeryItem index={index} item={item} handleClick={handleClick}></BakeryItem>
+          ))}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
-
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        </div>
+        <div className="Cart">
+          <h2>Cart</h2>
+          <p>Total: {total}</p>
+          <p>Cart Contents</p>
+          {cart.map((item, index) => (
+            
+            <p>{item.name}</p>
+          ))}
+          {/* <div>
+            for (let [item, count] of  Object.entries(cartDict)) {
+              <p>{this.item} {this.count}</p>
+            }
+          </div> */}
+        </div>
       </div>
     </div>
   );
